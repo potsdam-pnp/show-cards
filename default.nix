a42db0a3129d58e776518b66d0223729b38415d6
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-19 16:05:11 +0200

    add missing poppler_utils dependency

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
-{ buildNpmPackage, lib, ... }: 
+{ buildNpmPackage, lib, poppler_utils, ... }: 
 
 
 buildNpmPackage rec {
   pname = "show-cards";
   version = "0.1.0";
+
+  buildInputs = [ poppler_utils ];
 
   src = ./src;
   npmDepsHash = "sha256-veq64xSAgWXbbTemCg/jrAYVt4C30zNaYH2e/li+Yy8=";
