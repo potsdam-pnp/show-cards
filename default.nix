eee90bb6e338efd0bd3a518f81e1edd94c5a9eb8
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-19 16:40:22 +0200

    make pdftoppm available to execution of command

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
-{ buildNpmPackage, lib, poppler_utils, ... }: 
+{ buildNpmPackage, nodejs, poppler_utils, ... }: 
 
 
 buildNpmPackage rec {
   pname = "show-cards";
   version = "0.1.0";
 
-  buildInputs = [ poppler_utils ];
+  postInstall = ''
+    makeWrapper ${nodejs}/bin/node $out/bin/show-cards --add-flags $packageOut/index.js --prefix PATH : ${ poppler_utils }/bin
+  ''; 
 
   src = ./src;
   npmDepsHash = "sha256-veq64xSAgWXbbTemCg/jrAYVt4C30zNaYH2e/li+Yy8=";
